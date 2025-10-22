335c6ed6b2ebf9cf2affd125ab506dcf75a6d5e1
Mykyta Oliinyk <mykyta.oliinyk0@gmail.com>
2025-10-22 15:52:21 +0200

    connect prisma client

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class DatabaseService {}
+export class DatabaseService extends PrismaClient {
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+
+  async clearDatabase() {
+    await this.user.deleteMany(); 
+    await this.task.deleteMany();
+  }
+  
+  async enableShutdownHooks() {
+    process.on('beforeExit', async () => {
+      await this.$disconnect();
+    });
+}
+}
